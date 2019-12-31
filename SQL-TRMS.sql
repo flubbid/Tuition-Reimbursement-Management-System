d3fd7289bdc46fdaebd3656834e5a4d9b91b1a1f
flubbid <brendanwilson1@gmail.com>
2019-12-30 19:51:02 -0500

    adds foreign keys to Employee and DEPT TABLES

--- SQL-TRMS.sql
+++ SQL-TRMS.sql
@@ -12,28 +12,37 @@ LastName VARCHAR2(20),
 Email VARCHAR2(50) UNIQUE NOT NULL,
 password VARCHAR2(30) NOT NULL,
 dept_id NUMBER(10), --FK 2
-DirectSupervisor NUMBER(10),
-AvailableReimbursement NUMBER(10)
+supervisor NUMBER(10),
+AvailableReimbursement NUMBER(10),
+CONSTRAINT fk_employee_supervisor FOREIGN KEY(supervisor)
+REFERENCES employee(emp_id) ON DELETE CASCADE,
+CONSTRAINT fk_employee_dept FOREIGN KEY (dept_id)
+REFERENCES DEPT(dept_id) ON DELETE CASCADE
 );
 
 CREATE TABLE DEPT
 (
 dept_id NUMBER(10) PRIMARY KEY,
 deptName VARCHAR(20),
-deptHead VARCHAR(20) --FK
+deptHead VARCHAR(20),
+CONSTRAINT fk_dept_employee FOREIGN KEY (deptHead)
+REFERENCES employee(emp_id) ON DELETE CASCADE
+
 );
 
 CREATE TABLE Reimbursements
 (
 r_id NUMBER(10) PRIMARY KEY,
-emp_id NUMBER(10) , --FK
+emp_id NUMBER(10) , 
 reimTotAmount NUMBER(10),
 reimStatus NUMBER(10),
 reimAmtApproved NUMBER(10),
-dateSub Number(10), --look into date format
+dateSub DATE, 
 approvalDate Number(10),
 justification VARCHAR(3000),
-eventType NUMBER(10) --fk
+eventType NUMBER(10), --fk
+CONSTRAINT fk_reimbursements_employee REFERENCES emp_id
+REFERENCES employee(emp_id) ON DELETE CASCADE
 );
 
 --CREATE TABLE APPROVALS
@@ -62,6 +71,10 @@ grade_id NUMBER(10) PRIMARY KEY,
 gFormat VARCHAR(200),
 passing VARCHAR(10)
 );
+
+
+--=============================FOREIGN KEY CONSTRAINTS========================
+
 
 --Insert Values
 SELECT * FROM EMPLOYEE;
