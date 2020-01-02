ca87295568f9ed82acba2aaed03db243db77a643
flubbid <brendanwilson1@gmail.com>
2020-01-02 09:24:00 -0500

    updates SQL formatting

--- SQL-TRMS.sql
+++ SQL-TRMS.sql
@@ -3,7 +3,7 @@ COMMIT;
 DROP TABLE EMPLOYEE;
 
 --==================CREATE TABLES========================================
-
+SELECT * FROM EMPLOYEE;
 CREATE TABLE EMPLOYEE
 (
 emp_id NUMBER(10) PRIMARY KEY,--1
@@ -13,21 +13,20 @@ Email VARCHAR2(50) UNIQUE NOT NULL,
 password VARCHAR2(30) NOT NULL,
 dept_id NUMBER(10), --FK 2
 supervisor NUMBER(10),
-AvailableReimbursement NUMBER(10),
-CONSTRAINT fk_employee_supervisor FOREIGN KEY(supervisor)
-REFERENCES employee(emp_id) ON DELETE CASCADE,
-CONSTRAINT fk_employee_dept FOREIGN KEY (dept_id)
-REFERENCES DEPT(dept_id) ON DELETE CASCADE
+AvailableReimbursement NUMBER(10)
+--CONSTRAINT fk_employee_supervisor FOREIGN KEY(supervisor)
+--REFERENCES employee(emp_id) ON DELETE CASCADE,
+--CONSTRAINT fk_employee_dept FOREIGN KEY (dept_id)
+--REFERENCES DEPT(dept_id) ON DELETE CASCADE
 );
 
 CREATE TABLE DEPT
 (
 dept_id NUMBER(10) PRIMARY KEY,
 deptName VARCHAR(20),
-deptHead VARCHAR(20),
-CONSTRAINT fk_dept_employee FOREIGN KEY (deptHead)
-REFERENCES employee(emp_id) ON DELETE CASCADE
-
+deptHead VARCHAR(20)
+--CONSTRAINT fk_dept_employee FOREIGN KEY (deptHead)
+--REFERENCES employee(emp_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Reimbursements
@@ -41,8 +40,8 @@ dateSub DATE,
 approvalDate Number(10),
 justification VARCHAR(3000),
 eventType NUMBER(10), --fk
-CONSTRAINT fk_reimbursements_employee REFERENCES emp_id
-REFERENCES employee(emp_id) ON DELETE CASCADE
+--CONSTRAINT fk_reimbursements_employee REFERENCES emp_id
+--REFERENCES employee(emp_id) ON DELETE CASCADE
 );
 
 --CREATE TABLE APPROVALS
@@ -55,8 +54,8 @@ CREATE TABLE EVENTS
 evnt_id NUMBER(10) PRIMARY KEY,
 grade NUMBER(10), --FK
 name VARCHAR(200),
-beginDate NUMBER(10), --look into date format
-endDate NUMBER(10), --look into date format
+beginDate DATE, --look into date format
+endDate DATE, --look into date format
 evtType NUMBER(10), --FK
 time VARCHAR(8), --comeback to this
 city VARCHAR(30),
