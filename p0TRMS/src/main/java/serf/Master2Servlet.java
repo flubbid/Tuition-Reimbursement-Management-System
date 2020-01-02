2c54965b13bb77a3d621eaade0f229cc6eaf3916
flubbid <brendanwilson1@gmail.com>
2020-01-02 09:21:06 -0500

    adds Master Servlet and Request Helper

---
+++ p0TRMS/src/main/java/serf/Master2Servlet.java
@@ -0,0 +1,45 @@
+package serf;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Master2Servlet
+ */
+public class Master2Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Master2Servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			RequestHelper.process(request, response);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ p0TRMS/src/main/java/serf/RequestHelper.java
@@ -0,0 +1,51 @@
+package serf;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import webservice.EmployeeWebService;
+
+public class RequestHelper extends HttpServlet {
+
+
+public static void process(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
+		
+		//This method will delegate other methods
+		//on a different layer of our application
+		//to process the request.
+		
+		String uri = request.getRequestURI();
+		
+		System.out.println(uri);
+		
+		switch(uri) {
+			
+
+		
+		case "/p0TRMS/getEmployee.do": {
+			EmployeeWebService.getEmployee(request, response);
+			break;
+		}
+
+		case "/p0TRMS/addEmployee.do": {
+
+			break;
+		}
+		default: {
+			try {
+				response.sendError(404);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		}
+		
+	}
+	
+	 
+}
