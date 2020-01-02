41f9f84cb93529771a7631cc9b9fcc0e4dd5a403
flubbid <brendanwilson1@gmail.com>
2020-01-02 10:15:23 -0500

    updates tables, adds foreign keys, adds sequence generators

--- SQL-TRMS.sql
+++ SQL-TRMS.sql
@@ -13,66 +13,89 @@ Email VARCHAR2(50) UNIQUE NOT NULL,
 password VARCHAR2(30) NOT NULL,
 dept_id NUMBER(10), --FK 2
 supervisor NUMBER(10),
-AvailableReimbursement NUMBER(10)
---CONSTRAINT fk_employee_supervisor FOREIGN KEY(supervisor)
---REFERENCES employee(emp_id) ON DELETE CASCADE,
---CONSTRAINT fk_employee_dept FOREIGN KEY (dept_id)
---REFERENCES DEPT(dept_id) ON DELETE CASCADE
+AvailableReimbursement NUMBER(10),
+CONSTRAINT fk_employee_supervisor FOREIGN KEY(supervisor)
+REFERENCES employee(emp_id) ON DELETE CASCADE,
+CONSTRAINT fk_employee_dept FOREIGN KEY (dept_id)
+REFERENCES DEPT(dept_id) ON DELETE CASCADE
 );
 
 CREATE TABLE DEPT
 (
 dept_id NUMBER(10) PRIMARY KEY,
 deptName VARCHAR(20),
-deptHead VARCHAR(20)
---CONSTRAINT fk_dept_employee FOREIGN KEY (deptHead)
---REFERENCES employee(emp_id) ON DELETE CASCADE
+deptHead VARCHAR(20),
+CONSTRAINT fk_dept_employee FOREIGN KEY (deptHead)
+REFERENCES employee(emp_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Reimbursements
 (
 r_id NUMBER(10) PRIMARY KEY,
 emp_id NUMBER(10) , 
+grade_id NUMBER(10), --FK
 reimTotAmount NUMBER(10),
 reimStatus NUMBER(10),
 reimAmtApproved NUMBER(10),
 dateSub DATE, 
 approvalDate Number(10),
-justification VARCHAR(3000),
-eventType NUMBER(10), --fk
---CONSTRAINT fk_reimbursements_employee REFERENCES emp_id
---REFERENCES employee(emp_id) ON DELETE CASCADE
+justification VARCHAR2(3000),
+status VARCHAR2(30),
+evt_id NUMBER(10), --fk
+CONSTRAINT fk_reimbursements_employee FOREIGN KEY (emp_id)
+REFERENCES employee(emp_id) ON DELETE CASCADE,
+CONSTRAINT fk_reimbursements_eveny FOREIGN KEY (evt_id)
+REFERENCES events(evt_id) ON DELETE CASCADE,
+CONSTRAINT fk_reimbursement_grade FOREIGN KEY (grade_id)
+REFERENCES grades(grade_id) ON DELETE CASCADE
 );
 
---CREATE TABLE APPROVALS
---(
---
---);
+
 
 CREATE TABLE EVENTS
 (
-evnt_id NUMBER(10) PRIMARY KEY,
-grade NUMBER(10), --FK
+evnt_id NUMBER(10) PRIMARY KEY,reim_id NUMBER(10),
 name VARCHAR(200),
-beginDate DATE, --look into date format
-endDate DATE, --look into date format
-evtType NUMBER(10), --FK
+beginDate DATE, 
+endDate DATE, 
+evtType NUMBER(10), 
 time VARCHAR(8), --comeback to this
 city VARCHAR(30),
 state VARCHAR(30),
 cost NUMBER(10)
-
 );
 
 CREATE TABLE Grades
 (
 grade_id NUMBER(10) PRIMARY KEY,
+reim_id NUMBER(10), --FK
 gFormat VARCHAR(200),
-passing VARCHAR(10)
+passing VARCHAR(10),
+CONSTRAINT fk_grade_reimbursement FOREIGN KEY (reim_id)
+REFERENCES reimbursement(reim_id) ON DELETE CASCADE
 );
 
 
---=============================FOREIGN KEY CONSTRAINTS========================
+--=============================ID Sequence========================
+DROP SEQUENCE id_generator3;
+DROP SEQUENCE id_generator4;
+DROP SEQUENCE id_generator5;
+
+CREATE SEQUENCE id_generator3
+        MINVALUE 1
+        START WITH 1
+        INCREMENT BY 1;
+CREATE SEQUENCE id_generator4
+        MINVALUE 1
+        START WITH 1
+        INCREMENT BY 1;
+CREATE SEQUENCE id_generator5
+        MINVALUE 1
+        START WITH 1
+        INCREMENT BY 1;
+
+-- =====================================CREATE/CALL PROCEDURS ==========================================
+
 
 
 --Insert Values
