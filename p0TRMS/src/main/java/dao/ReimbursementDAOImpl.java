b84cb8de64af2b3d79d1856cfd98b04b31151e0a
flubbid <brendanwilson1@gmail.com>
2020-01-04 06:57:30 -0500

    adds reimbursemtnDAOimpl | getReimbursement working with jdbc

---
+++ p0TRMS/src/main/java/dao/ReimbursementDAOImpl.java
@@ -0,0 +1,69 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import model.Event;
+import model.Reimbursement;
+import utils.JDBC;
+
+public class ReimbursementDAOImpl implements ReimbursementDAO {
+
+	public static Connection conn = JDBC.getConnection();
+	
+	@Override
+	public Reimbursement getReimbursement(int reim_id) {
+		try {
+			String sql = "SELECT * FROM Event WHERE evt_id = ?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1, reim_id);
+			
+			ResultSet rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				return new Reimbursement(
+						rs.getInt("reim_id"),
+						rs.getInt("emp_id"),
+						rs.getInt("grade_id"),
+						rs.getInt("evt_id"),
+						rs.getDouble("reimTotAmount"),
+						rs.getDouble("reimAmtApproved"),
+						rs.getString("reimStatus"),
+						rs.getString("dateSub"),
+						rs.getString("approvalDate"),
+						rs.getString("justification")
+						);
+						
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+	
+
+	@Override
+	public void createReimbursement(Reimbursement reim) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Reimbursement> getAllReimbursement() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Reimbursement updateReimbursement(int reim_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
