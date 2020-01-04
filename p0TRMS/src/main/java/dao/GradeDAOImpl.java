b8e89f9fc353824fd56c9b0cc9980b301e0408e3
flubbid <brendanwilson1@gmail.com>
2020-01-04 07:17:15 -0500

    adds GradeDAOImpl | creates getGrade method

---
+++ p0TRMS/src/main/java/dao/GradeDAOImpl.java
@@ -0,0 +1,62 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import model.Event;
+import model.Grade;
+import utils.JDBC;
+
+public class GradeDAOImpl implements GradeDAO {
+
+public static Connection conn = JDBC.getConnection();
+	
+	@Override
+	public Grade getGrade(int grade_id) {
+		try {
+			String sql = "SELECT * FROM Event WHERE evt_id = ?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1, grade_id);
+			
+			ResultSet rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				return new Grade(
+						rs.getInt("grade_ID"),
+						rs.getString("GFORMAT"),
+						rs.getString("PASSING")
+
+						);
+						
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+
+	}
+
+	@Override
+	public Grade createGrade(Grade grade) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Grade> getAllGrades() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateGrade(int grade_id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
